9557af86a63d7546ff583778d100f1875cb0269d
Olga Papkovskaya <o.papkovskaya@gmail.com>
2018-02-21 01:24:37 +0300

    done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var n = 0;
+  for (var i = 0; i < preferences.length; i++){
+   var id1 = preferences[i];
+   var id2 = preferences[id1-1];
+   var id3 = preferences[id2-1];
+   if (i+1 == id3 && i+1 != id2){
+     n++;
+     preferences[i] = 0;
+     preferences[id1 - 1] = 0;
+     preferences[id2 - 1] = 0;
+   }
+  }
+  return n;
 };
